c1f64334b7275a312a9305421c4b3785ba4afe81
MFALLAM <mohammed.fathi.allam@gmail.com>
2021-07-10 23:32:57 +0300

    Урок 6. Продвинутое ООП - практическое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import animals.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        Dog dog = new Dog(500, 10, 0.5);
+        System.out.println("animals.Dog run " + dog.run(120));
+        System.out.println("animals.Dog jump " + dog.jump(1));
+        System.out.println("animals.Dog swim " + dog.swim(10));
+
+        System.out.println("\t");
+
+        Cat cat = new Cat(200, 0, 2);
+        System.out.println("animals.Cat run " + cat.run(100));
+        System.out.println("animals.Cat jump " + cat.jump(1));
+        System.out.println("animals.Cat swim " + cat.swim(10));
 
     }
 }
---
+++ src/animals/Cat.java
@@ -0,0 +1,32 @@
+package animals;
+
+public class Cat extends Animals {
+
+    private final int run;
+
+    private final int swim;
+
+    private final double jump;
+
+    public Cat(int run, int swim, double jump) {
+        super(run, 0, jump);
+        this.run = run;
+        this.swim = swim;
+        this.jump = jump;
+    }
+
+    @Override
+    public boolean run(int length) {
+        return (length == this.run || length < this.run) ? true : false;
+    }
+
+    @Override
+    public boolean swim(int length) {
+        return false;
+    }
+
+    @Override
+    public boolean jump(double height) {
+        return (height == this.jump || height < this.jump) ? true : false;
+    }
+}
